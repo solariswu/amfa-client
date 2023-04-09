ea00b61d3c5c081e8e07322fc1365708c6f4c3ea
Yungang <yunganw@amazon.com>
2023-04-09 16:25:00 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import { Amplify, Auth, Hub } from "aws-amplify";
+
+import awsconfig from "./aws-export";
+
+Amplify.configure(awsconfig);
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          setLoading(false);
+          setUser(data);
+          break;
+        case "signOut":
+          setLoading(false);
+          setUser(null);
+          break;
+        default:
+          break;
+      }
+    });
+
+    setLoading(true);
+    Auth.currentAuthenticatedUser()
+      .then((currentUser) => {
+        setUser(currentUser);
+        console.log(user);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        console.log("Not signed in");
+      });
+
+    return unsubscribe;
+  }, []);
+
+  const USER = (user) => {
+    return (
+      <div>
+        <span>User: {user.attributes.email}</span>
+        <br />
+        <button className="button-paper" onClick={() => Auth.signOut()}>
+          Logout
+        </button>
+      </div>
+    );
+  };
+
+  const LOGIN = () => {
+    return (
+      <button
+        className="button-paper"
+        onClick={() => Auth.federatedSignIn({ provider: "amfa" })}
+      >
+        Login
+      </button>
+    );
+  };
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <div style={{ position: "absolute", top: "48%" }}>
+          {user && user.attributes ? <USER user={user} /> : <LOGIN />}
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/aws-export.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+const awsmobile = {
+  aws_project_region: "eu-west-1",
+  aws_user_pools_web_client_id: "61uc0k2m4rvckn5gvn78v8p62d",
+  aws_user_pools_id: "eu-west-1_cXCwPjWWC",
+  oauth: {
+    domain: "epnd-dev001-amfa.auth.eu-west-1.amazoncognito.com",
+    scope: ["openid", "profile", "aws.cognito.signin.user.admin"],
+    redirectSignIn: "https://oi99c3.csb.app/",
+    redirectSignOut: "https://oi99c3.csb.app/",
+    responseType: "code"
+  }
+};
+
+export default awsmobile;
