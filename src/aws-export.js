dd95838e50020b8b13417a4a87965ec6e5908478
Yungang <yunganw@amazon.com>
2023-04-10 15:22:54 +1000

    update userpool id to 0410

--- src/aws-export.js
+++ src/aws-export.js
@@ -2,8 +2,8 @@
 
 const awsmobile = {
   aws_project_region: "eu-west-1",
-  aws_user_pools_web_client_id: "61uc0k2m4rvckn5gvn78v8p62d",
-  aws_user_pools_id: "eu-west-1_cXCwPjWWC",
+  aws_user_pools_web_client_id: "6fm1sv9l4rhkg47biq7hl0vhp4",
+  aws_user_pools_id: "eu-west-1_KKXOfv8rW",
   oauth: {
     domain: "epnd-dev001-amfa.auth.eu-west-1.amazoncognito.com",
     scope: ["openid", "profile", "aws.cognito.signin.user.admin"],
