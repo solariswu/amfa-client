7e157a02c85d06a4a40dc5faf261f353c833f5d4
Yungang <yunganw@amazon.com>
2023-04-10 15:28:05 +1000

    correct domain

--- src/aws-export.js
+++ src/aws-export.js
@@ -5,7 +5,7 @@ const awsmobile = {
   aws_user_pools_web_client_id: "6fm1sv9l4rhkg47biq7hl0vhp4",
   aws_user_pools_id: "eu-west-1_KKXOfv8rW",
   oauth: {
-    domain: "epnd-dev001-amfa.auth.eu-west-1.amazoncognito.com",
+    domain: "epnd-dev002-amfa.auth.eu-west-1.amazoncognito.com",
     scope: ["openid", "profile", "aws.cognito.signin.user.admin"],
     redirectSignIn: "https://amfa.netlify.app/",
     redirectSignOut: "https://amfa.netlify.app/",
