6bf3bdda3293cade4f27b3224602ab93f1441faa
Yungang <yunganw@amazon.com>
2023-04-17 17:13:42 +1000

    update with account0417

--- src/aws-export.js
+++ src/aws-export.js
@@ -2,10 +2,10 @@
 
 const awsmobile = {
   aws_project_region: "eu-west-1",
-  aws_user_pools_web_client_id: "3h0rirecom4pf84i79mcigl1o7",
-  aws_user_pools_id: "eu-west-1_KKXOfv8rW",
+  aws_user_pools_web_client_id: "1eq93n0cbmugflglr65s9cndjk",
+  aws_user_pools_id: "eu-west-1_4H3SVoiDT",
   oauth: {
-    domain: "epnd-dev002-amfa.auth.eu-west-1.amazoncognito.com",
+    domain: "epnd-dev003-amfa.auth.eu-west-1.amazoncognito.com",
     scope: ["openid", "profile", "aws.cognito.signin.user.admin"],
     redirectSignIn: "https://amfa.netlify.app/",
     redirectSignOut: "https://amfa.netlify.app/",
