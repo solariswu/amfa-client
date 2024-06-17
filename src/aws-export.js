c51f2ac93cfa9431a005b6bcba7f7b58718ce375
Yungang <mycherrier@gmail.com>
2024-06-17 22:25:17 +1000

    update app client id

--- src/aws-export.js
+++ src/aws-export.js
@@ -2,7 +2,7 @@
 
 const awsmobile = {
   aws_project_region: "eu-west-1",
-  aws_user_pools_web_client_id: "3g7lq4mtbblujc7k6o7af5110",
+  aws_user_pools_web_client_id: "2ncc809ahspj9rk8hhm6tfae9h",
   aws_user_pools_id: "eu-west-1_vitRQ9sum",
   oauth: {
     domain: "amfa-dev004-apersona.auth.eu-west-1.amazoncognito.com",
