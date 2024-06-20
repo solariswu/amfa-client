970ba92d017643976cc50abfa051e864bf8c647f
Yungang <mycherrier@gmail.com>
2024-06-20 12:09:13 +1000

    remove cognito admin scope

--- src/aws-export.js
+++ src/aws-export.js
@@ -6,7 +6,7 @@ const awsmobile = {
   aws_user_pools_id: "eu-west-1_vitRQ9sum",
   oauth: {
     domain: "amfa-dev004-apersona.auth.eu-west-1.amazoncognito.com",
-    scope: ["openid", "profile", "aws.cognito.signin.user.admin"],
+    scope: ["openid", "profile"],
     redirectSignIn: "https://amfa.netlify.app/",
     redirectSignOut: "https://amfa.netlify.app/",
     responseType: "code"
