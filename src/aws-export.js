3e43f3ac169d029b639dc5028aff4dfafa6ed38f
Yungang <mycherrier@gmail.com>
2025-01-16 12:33:39 +1100

    Update aws-export.js

--- src/aws-export.js
+++ src/aws-export.js
@@ -2,10 +2,10 @@
 
 const awsmobile = {
   aws_project_region: "eu-west-1",
-  aws_user_pools_web_client_id: "2ncc809ahspj9rk8hhm6tfae9h",
-  aws_user_pools_id: "eu-west-1_vitRQ9sum",
+  aws_user_pools_web_client_id: "1jcljjua7vqrl7madf1cak1sgg",
+  aws_user_pools_id: "eu-west-1_grTmuNlJb",
   oauth: {
-    domain: "amfa-dev004-apersona.auth.eu-west-1.amazoncognito.com",
+    domain: "ev120-o7kfs.auth.eu-west-1.amazoncognito.com",
     scope: ["openid", "profile"],
     redirectSignIn: "https://amfa.netlify.app/",
     redirectSignOut: "https://amfa.netlify.app/",
