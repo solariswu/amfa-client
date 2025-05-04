7bfb4082699932e3a0007f6b203df192ebd6cb61
Yungang <mycherrier@gmail.com>
2025-05-04 15:47:49 +1000

    Update aws-export.js

--- src/aws-export.js
+++ src/aws-export.js
@@ -2,10 +2,10 @@
 
 const awsmobile = {
   aws_project_region: "eu-west-1",
-  aws_user_pools_web_client_id: "1jcljjua7vqrl7madf1cak1sgg",
-  aws_user_pools_id: "eu-west-1_grTmuNlJb",
+  aws_user_pools_web_client_id: "4lmobv1ttc44s0ooqf2hp48hqm",
+  aws_user_pools_id: "eu-west-1_NNqzdEAgB",
   oauth: {
-    domain: "ev120-o7kfs.auth.eu-west-1.amazoncognito.com",
+    domain: "dev666-o7kfs.auth.eu-west-1.amazoncognito.com",
     scope: ["openid", "profile"],
     redirectSignIn: "https://amfa.netlify.app/",
     redirectSignOut: "https://amfa.netlify.app/",
